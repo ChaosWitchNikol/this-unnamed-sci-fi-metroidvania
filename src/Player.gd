56fa5f4480b7b139f557a2bdfa531d4db6e14ee1
NikkiC <cupi.niki@gmail.com>
2019-04-03 19:43:19 +0200

    added basic player controls

--- src/Player.gd
+++ src/Player.gd
@@ -1,13 +1,50 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const FLOOR_VECTOR : Vector2 = Vector2(0, -1)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+const GRAVITY : float = 9.8
+const MASS : float = 20.0
+const MOVEMENT_SPEED : int = 120
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+const JUMP_FORCE : float = 70.0
+const ALLOWED_JUMPS : int = 2
+
+
+
+var linear_velocity : Vector2 = Vector2()
+var jumps_count : int = 0
+
+
+
+
+func _physics_process(delta : float) -> void:
+	
+	#	calculate gravity
+	linear_velocity.y +=  delta * GRAVITY * MASS
+	#	reset y when on the ground
+	if is_on_floor():
+		linear_velocity.y = 0
+		jumps_count = 0
+	if is_on_ceiling():
+		linear_velocity.y = 0
+	if is_on_wall():
+		jumps_count = 0
+		if linear_velocity.y > 0:
+			linear_velocity.y = linear_velocity.y / 2.0
+	
+	if Input.is_action_just_pressed("ui_select"):
+		if jumps_count < ALLOWED_JUMPS:
+			linear_velocity.y = -JUMP_FORCE
+			jumps_count += 1
+		
+	
+	linear_velocity.x = 0
+	if Input.is_action_pressed("ui_left"):
+		linear_velocity.x = -MOVEMENT_SPEED
+	if Input.is_action_pressed("ui_right"):
+		linear_velocity.x = MOVEMENT_SPEED
+	
+	
+	move_and_slide(linear_velocity, FLOOR_VECTOR, true)
+
+
