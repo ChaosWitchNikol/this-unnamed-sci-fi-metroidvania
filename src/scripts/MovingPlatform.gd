9c7b98560fd31d3910d2ae0df21bdd44818d2ae3
NikkiC <cupi.niki@gmail.com>
2019-04-14 16:11:39 +0200

    moved all assets into subfolders

--- src/scripts/MovingPlatform.gd
+++ src/scripts/MovingPlatform.gd
@@ -1,12 +1,14 @@
+tool
 extends Node2D
 
 const PointScene = preload("res://MovingPlatformPoint.tscn")
 
-onready var Body : KinematicBody2D = get_node("Body")
 
 #	export variables
 export var MOVEMENT_SPEED : float = 40.0
 export var ONE_WAY_COLLISION : bool = false 
+export (int, 2, 16) var SIZE_X : int = 4
+export (int, 1, 4) var SIZE_Y : int = 1
 
 #	point variables
 var all_points : Array
@@ -25,22 +27,10 @@ func _ready() -> void:
 	# get all points and sort them by order number
 	all_points = Utils.get_node_children_by_type(self, "Position2D")
 	all_points = Utils.sort_nodes_by_order_number(all_points)
-	for point in all_points:
-		print(point, point.ORDER_NUMBER)
 	
 	# set statrting moving direction
 	set_direction_vector()
-	
-	# set collision being one way
-	Body.get_node("Collision").one_way_collision = ONE_WAY_COLLISION
 
-func _physics_process(delta) -> void:
-	
-#	Body.move_and_slide(direction * MOVEMENT_SPEED)
-	Body.position += direction * MOVEMENT_SPEED * delta
-	if Body.position.distance_to(get_next_point_position()) < 1.0:
-		set_next_target_point()
-		set_direction_vector()
 
 
 func get_next_point_position() -> Vector2:
@@ -58,3 +48,8 @@ func set_next_target_point():
 	next_point_index = current_point_index + 1
 	if next_point_index >= all_points.size():
 		next_point_index = 0
+
+
+
+
+
---
+++ src/scripts/MovingPlatformBody.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+onready var P = get_owner()
+
+func _ready():
+	$Collision.shape.extents.x = P.SIZE_X * Const.TILE_HALF_SIZE
+	$Collision.shape.extents.y = P.SIZE_Y * Const.TILE_HALF_SIZE
+	$Collision.one_way_collision = P.ONE_WAY_COLLISION
+
+func _physics_process(delta):
+	position += P.direction * P.MOVEMENT_SPEED * delta
+	if position.distance_to(P.get_next_point_position()) < 1.0:
+		P.set_next_target_point()
+		P.set_direction_vector()
