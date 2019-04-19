d2530045a306790c859b3f378f0d5d0ee81b81eb
NikkiC <cupi.niki@gmail.com>
2019-04-19 16:43:55 +0200

    snaping on slopes using safe margins

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 #	DEFINE constants
 const FLOOR_VECTOR : Vector2 = Vector2(0, -1)
-const SNAP_VECTOR : Vector2 = Vector2(0, 8)
+const SNAP_VECTOR : Vector2 = Vector2(0, 4)
 const MAX_SLOPE_DEGREE : float = deg2rad(50)
 const JUMP_SNAP_VECTOR : Vector2 = Vector2(0, 0)
 
@@ -51,7 +51,6 @@ func _physics_process(delta : float) -> void:
 			linear_velocity.y = linear_velocity.y / 1.25	# divide linear velocity by a factor
 	
 	
-	
 	if Input.is_action_just_pressed("ui_select"):
 		if can_jump and jumps_count < ALLOWED_JUMPS :
 			linear_velocity.y = -JUMP_FORCE
@@ -66,11 +65,6 @@ func _physics_process(delta : float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		linear_velocity.x = MOVEMENT_SPEED
 	
-	# calculate snap_vector x component based on
-	if $RayRight.is_colliding():
-		snap_vector.x += 8
-	if $RayLeft.is_colliding():
-		snap_vector.x -= 8
 	
 	if is_on_ceiling():
 		linear_velocity.y += GRAVITY
