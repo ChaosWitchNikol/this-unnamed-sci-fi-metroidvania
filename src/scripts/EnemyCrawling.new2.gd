2079cdb9164cdb4b1d32d2d096cecfecd2fecf9d
NikkiC <cupi.niki@gmail.com>
2019-07-01 09:44:22 +0200

    working inner circle crawler

--- src/scripts/EnemyCrawling.new2.gd
+++ src/scripts/EnemyCrawling.new2.gd
@@ -1,8 +1,13 @@
 extends Enemy
 class_name EnemyCrawlingNew2
 
+onready var FeelFront : RayCast2D = get_node("FeelFront")
+
+
 const ROT_INNER : int = -1
 const ROT_OUTER : int = 1
+
+
 
 var forward_vector : Vector2
 
@@ -15,26 +20,27 @@ func _ready() -> void:
 #	processors
 ## @Override
 func process_movement(delta : float) -> void:
-	var collides : Array = [is_on_floor(), is_on_wall(), is_on_ceiling()]
+	var collides : Array = [is_on_floor(), is_on_wall()]
 	var collides_count : int = collides.count(true)
 	$Body2.disabled = $GroundMid.is_colliding()
 	
 	var grounds : Array = [$GroundBack.is_colliding(), $GroundMid.is_colliding(), $GroundFront.is_colliding()]
 	var grounds_count : int = grounds.count(true)
 	
-	if collides_count == 1:
+	if grounds_count == 0 and $FeelFront.is_colliding():
+		self.flip_facing()
+	
+	if is_on_floor():
 		if grounds_count == 0:
 			process_rotation(ROT_OUTER)
 			return
-	elif collides_count > 1:
-		if not $Back.is_colliding():
-			process_rotation(ROT_INNER)
-		
-			return
-#	print($Back.is_colliding())
-	if $Back.is_colliding() or grounds_count >= 1:
+	
+	if $FeelFront.is_colliding():
+		process_rotation(ROT_INNER)
+		return
+
+	if grounds_count >= 1:
 		process_forward()
-		print("move forward ")
 		
 
 func process_rotation(rotation_type : int ) -> void:
@@ -47,9 +53,9 @@ func process_rotation(rotation_type : int ) -> void:
 	elif test_rotation_degrees > 360:
 		test_rotation_degrees = round(test_rotation_degrees - 360.0)
 	
-#	if test_move(Transform2D(test_rotation_degrees, position), position):
-	rotation_degrees = test_rotation_degrees
-	calc_next_gravity_and_forward_vector(rotation_type)
+	if test_move(Transform2D(test_rotation_degrees, position), position):
+		rotation_degrees = test_rotation_degrees
+		calc_next_gravity_and_forward_vector(rotation_type)
 
 func process_forward() -> void:
 	if abs(forward_vector.x) > 0:
@@ -59,11 +65,19 @@ func process_forward() -> void:
 
 func calc_next_gravity_and_forward_vector(rotation_type : int) -> void:
 	forward_vector = gravity_vector * rotation_type
-	gravity_vector = gravity_vector.rotated(PI / 2 * facing * rotation_type).round()
+	self.gravity_vector = gravity_vector.rotated(PI / 2 * facing * rotation_type).round()
 
 
 func calc_forward_vector() -> void:
 	forward_vector = gravity_vector.rotated(-PI / 2 * facing).round()
+
+
+#	life
+##	@Override
+func flip_facing() -> void:
+	.flip_facing()
+	FeelFront.position.x = abs(FeelFront.position.x) * facing
+	FeelFront.cast_to.x = abs(FeelFront.cast_to.x) * facing
 
 ################################
 #	Setters
