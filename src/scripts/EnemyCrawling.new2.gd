197a12d34afb89baac68fb2dde73fc631f1e1f6f
NikkiC <cupi.niki@gmail.com>
2019-07-01 06:12:59 +0200

    removed prints

--- src/scripts/EnemyCrawling.new2.gd
+++ src/scripts/EnemyCrawling.new2.gd
@@ -14,36 +14,32 @@ func _ready() -> void:
 ## @Override
 func process_movement(delta : float) -> void:
 	if is_on_floor() or is_on_wall() or is_on_ceiling():
-		rotation_degrees= round(rotation_degrees)
-#		print($GroundBack.is_colliding(), $GroundMid.is_colliding(), $GroundFront.is_colliding())
+		if $Face.is_colliding():
+			print("face collidign")
 		$Body2.disabled = $GroundMid.is_colliding()
 		if $GroundBack.is_colliding() or $GroundMid.is_colliding() or $GroundFront.is_colliding():
 			if abs(forward_vector.x) > 0:
-#				print("move x ", forward_vector.x)
 				linear_velocity.x = MOVEMENT_SPEED * forward_vector.x
 			elif abs(forward_vector.y) > 0:
-#				print("move y ", forward_vector.y)
 				linear_velocity.y = MOVEMENT_SPEED * forward_vector.y
 		else:
 			linear_velocity = Vector2()
 			position = position.round()
 			var rot : float = rotation_degrees
 			var test_rotation : float = round(rotation_degrees + 90)
+			
 			if test_rotation < 0:
 				test_rotation = round(360.0 + test_rotation)
-			print(test_rotation)
+			
 			var test = test_move(Transform2D(test_rotation, position), position)
 			if test:
 				rotation_degrees = test_rotation
 				gravity_vector = gravity_vector.rotated(PI / 2 * facing).round()
-				print("> ", gravity_vector)
 				calc_forward_vector()
-				print(" >", rotation_degrees)
 
 
 func calc_forward_vector() -> void:
 	forward_vector = gravity_vector.rotated(-PI / 2 * facing).round()
-	print(gravity_vector, " ", forward_vector)
 
 ################################
 #	Setters
@@ -55,7 +51,3 @@ func set_facing(value: int = FacingDirs.RIGHT) -> void:
 
 
 
-func _on_Bottom_body_exited(body: PhysicsBody2D) -> void:
-	forward_vector = Vector2()
-	print("body exited")
-	pass # Replace with function body.
