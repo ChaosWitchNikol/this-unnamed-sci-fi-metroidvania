fa77492370e6fee9314231d9564546bb787c7a42
NikkiC <cupi.niki@gmail.com>
2019-07-01 07:37:25 +0200

    inner crawling rotating

--- src/scripts/EnemyCrawling.new2.gd
+++ src/scripts/EnemyCrawling.new2.gd
@@ -1,41 +1,65 @@
 extends Enemy
 class_name EnemyCrawlingNew2
 
-#export var(float, 0, 360, 90) 
+const ROT_INNER : int = -1
+const ROT_OUTER : int = 1
 
 var forward_vector : Vector2
 
 
 func _ready() -> void:
 	calc_forward_vector()
+
 ################################
 #	Custom functions
 #	processors
 ## @Override
 func process_movement(delta : float) -> void:
-	if is_on_floor() or is_on_wall() or is_on_ceiling():
-		if $Face.is_colliding():
-			print("face collidign")
-		$Body2.disabled = $GroundMid.is_colliding()
-		if $GroundBack.is_colliding() or $GroundMid.is_colliding() or $GroundFront.is_colliding():
-			if abs(forward_vector.x) > 0:
-				linear_velocity.x = MOVEMENT_SPEED * forward_vector.x
-			elif abs(forward_vector.y) > 0:
-				linear_velocity.y = MOVEMENT_SPEED * forward_vector.y
-		else:
-			linear_velocity = Vector2()
-			position = position.round()
-			var rot : float = rotation_degrees
-			var test_rotation : float = round(rotation_degrees + 90)
-			
-			if test_rotation < 0:
-				test_rotation = round(360.0 + test_rotation)
-			
-			var test = test_move(Transform2D(test_rotation, position), position)
-			if test:
-				rotation_degrees = test_rotation
-				gravity_vector = gravity_vector.rotated(PI / 2 * facing).round()
-				calc_forward_vector()
+	var collides : Array = [is_on_floor(), is_on_wall(), is_on_ceiling()]
+	var collides_count : int = collides.count(true)
+	$Body2.disabled = $GroundMid.is_colliding()
+	
+	var grounds : Array = [$GroundBack.is_colliding(), $GroundMid.is_colliding(), $GroundFront.is_colliding()]
+	var grounds_count : int = grounds.count(true)
+	
+	if collides_count == 1:
+		if grounds_count == 0:
+			process_rotation(ROT_OUTER)
+			return
+	elif collides_count > 1:
+		if not $Back.is_colliding():
+			process_rotation(ROT_INNER)
+		
+			return
+#	print($Back.is_colliding())
+	if $Back.is_colliding() or grounds_count >= 1:
+		process_forward()
+		print("move forward ")
+		
+
+func process_rotation(rotation_type : int ) -> void:
+	linear_velocity = Vector2()
+	position = position.round()
+	
+	var test_rotation_degrees : float = round(rotation_degrees + 90 * rotation_type)
+	if test_rotation_degrees < 0:
+		test_rotation_degrees = round(360.0 + test_rotation_degrees)
+	elif test_rotation_degrees > 360:
+		test_rotation_degrees = round(test_rotation_degrees - 360.0)
+	
+#	if test_move(Transform2D(test_rotation_degrees, position), position):
+	rotation_degrees = test_rotation_degrees
+	calc_next_gravity_and_forward_vector(rotation_type)
+
+func process_forward() -> void:
+	if abs(forward_vector.x) > 0:
+		linear_velocity.x = MOVEMENT_SPEED * forward_vector.x
+	if abs(forward_vector.y) > 0:
+		linear_velocity.y = MOVEMENT_SPEED * forward_vector.y
+
+func calc_next_gravity_and_forward_vector(rotation_type : int) -> void:
+	forward_vector = gravity_vector * rotation_type
+	gravity_vector = gravity_vector.rotated(PI / 2 * facing * rotation_type).round()
 
 
 func calc_forward_vector() -> void:
